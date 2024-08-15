60dd24e609a9b9304c3cb4618f83eda683035de1
M.Usman <youngboys920842@gmail.com>
2024-08-16 01:53:46 +0500

    first commit

--- training11/settings.py
+++ training11/settings.py
@@ -117,13 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where static files will be collected
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Additional static file locations
-]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URLS='/media/'
 MEDIA_ROOT=BASE_DIR /'media'
